316b127abe7311a65d5d3057af2f2307bab7e79d
kishor400 <kishor404@outlook.in>
2024-04-12 07:44:09 +0530

    Responsive Nav

--- Script/script.js
+++ Script/script.js
@@ -33,3 +33,14 @@ setSlide();
 let intervalId = setInterval(nextSlide, 7000); 
 
 
+const opnNav=()=>{
+    console.log("Open NAv")
+    document.querySelector("#Head-Nav-List").style="transform: translate(0px);";
+    document.querySelector(".Head-Nav-x").style="display:flex;";
+    document.querySelector(".Head-Nav-3").style="display:none;";
+}
+const clsNav=()=>{
+    document.querySelector("#Head-Nav-List").style="transform: translate(400px);";
+    document.querySelector(".Head-Nav-x").style="display:none;";
+    document.querySelector(".Head-Nav-3").style="display:flex;";
+}
