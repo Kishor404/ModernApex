c6c747ec15c662da23cb6d3ba574c4351a007001
kishor400 <kishor404@outlook.in>
2024-04-18 16:25:39 +0530

    index

--- Script/script.js
+++ Script/script.js
@@ -4,15 +4,16 @@ const nextSlide=()=>{
     document.querySelector(slides[id]).style="display:none;"
     id=(id+1)%slides.length;
     document.querySelector(slides[id]).style="display:flex;"
+    typeText()
 }
 const prevSlide=()=>{
     document.querySelector(slides[id]).style="display:none;"
     id=(id-1+slides.length)%slides.length;
     console.log(id)
     document.querySelector(slides[id]).style="display:flex;"
+    typeText()
 }
 
-let intervalId = setInterval(nextSlide, 7000); 
 
 
 const opnNav=()=>{
@@ -26,3 +27,35 @@ const clsNav=()=>{
     document.querySelector(".Head-Nav-x").style="display:none;";
     document.querySelector(".Head-Nav-3").style="display:flex;";
 }
+
+
+// Multiline text content to be typed out
+const multilineText = [
+    "Welcome to Modern Apex Electromechanical Works !",
+    "We provide top-quality services for maintenance, testing, troubleshooting, and installation.",
+    "Contact us today for more information."
+];
+
+const typingSpeed = 40; // Typing speed in milliseconds
+
+const typingContainer = document.getElementsByClassName("typing-text");
+console.log(typingContainer)
+let charIndex = 0;
+
+function typeText() {
+
+        if (charIndex <= multilineText[id].length) {
+            typingContainer[id].textContent = multilineText[id].slice(0, charIndex);
+            charIndex++;
+            setTimeout(typeText, typingSpeed);
+        } else {
+            charIndex = 0;
+            setTimeout(nextSlide,2000)
+        }
+
+}
+
+// Start typing effect when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+    typeText();
+});
