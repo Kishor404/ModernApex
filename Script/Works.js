9d2adb2d435ef231b08be5f4ee620b256922f682
kishor400 <kishor404@outlook.in>
2024-05-20 14:41:20 +0530

    Testing 1 Page

--- Script/Works.js
+++ Script/Works.js
@@ -1,34 +1,3 @@
-let Slide = [
-    ["../Assets/Slider/Slider1.jpeg"],
-    ["../Assets/Slider/Slider2.jpeg"],
-    ["../Assets/Slider/Slider3.jpeg"]
-];
-
-let id_Slide = 0;
-const sliderImg = document.querySelector(".Hox-Slider-Img");
-
-const prevSlide = () => {
-    id_Slide = (id_Slide - 1 + Slide.length) % Slide.length;
-    setSlide();
-};
-
-const nextSlide = () => {
-    id_Slide = (id_Slide + 1) % Slide.length;
-    setSlide();
-};
-
-const setSlide = () => {
-    sliderImg.style.opacity = 0.8;
-    setTimeout(() => {
-        sliderImg.src = Slide[id_Slide][0];
-        sliderImg.style.opacity = 1;
-    }, 500);
-};
-setSlide();
-
-let intervalId = setInterval(nextSlide, 7000); 
-
-
 const opnNav=()=>{
     console.log("Open NAv")
     document.querySelector("#Head-Nav-List").style="transform: translate(0px);";
